88e1d5e91e8f74c8bc55a7e706036ea8a57dfea1
kirlozavr <kirill.protchenko@mail.ru>
2023-06-04 22:32:09 +1000

    Класс для вызова камеры и получения результата с нее

---
+++ app/src/main/java/com/example/boundingboxesdetector/utils/CameraUtil.kt
@@ -0,0 +1,47 @@
+package com.example.boundingboxesdetector.utils
+
+import android.content.Intent
+import android.net.Uri
+import android.provider.MediaStore
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
+import com.example.boundingboxesdetector.BuildConfig
+import java.io.File
+
+class CameraUtil constructor(private val activity: AppCompatActivity){
+
+    private lateinit var compress: CompressImage
+
+    private lateinit var uri: Uri
+    private val activityResultLauncher = activity.registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult(),
+        ActivityResultCallback {
+            if (it.resultCode == AppCompatActivity.RESULT_OK){
+                compress.compress(activity.applicationContext, uri)
+            }
+        })
+
+    fun launch(compress: CompressImage){
+        this.compress = compress
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        uri = FileProvider.getUriForFile(
+            activity,
+            BuildConfig.APPLICATION_ID + ".provider",
+            createImageFile()
+        )
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, uri)
+        activityResultLauncher.launch(intent)
+    }
+
+    private fun createImageFile(): File{
+        val storageDir = activity.getExternalFilesDir(null)
+
+        return File.createTempFile(
+            "picture",
+            ".jpg",
+            storageDir
+        )
+    }
+}
