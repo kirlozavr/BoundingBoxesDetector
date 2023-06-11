68d4092b742a8423065a1aab9f1aa06df4aece31
kirlozavr <kirill.protchenko@mail.ru>
2023-06-11 11:49:54 +1000

    Добавил класс для сохранения изображения

---
+++ app/src/main/java/com/example/boundingboxesdetector/utils/SaveImageUtil.kt
@@ -0,0 +1,28 @@
+package com.example.boundingboxesdetector.utils
+
+import android.graphics.Bitmap
+import android.os.Environment
+import java.io.File
+import java.io.FileOutputStream
+
+object SaveImageUtil {
+
+    suspend fun save(bitmap: Bitmap): Boolean {
+        return try {
+            val filePath =
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+            val dir = File(filePath.absolutePath + "/BoundingBoxes/")
+            dir.mkdirs()
+            val file = File(dir, "${System.currentTimeMillis()}.jpg")
+            val outputStream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+            outputStream.flush()
+            outputStream.close()
+            bitmap.recycle()
+            true
+        } catch (e: Exception) {
+            e.printStackTrace()
+            false
+        }
+    }
+}
